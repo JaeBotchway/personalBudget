a6e45fb1d78fc8e40a8075d899e6625b08efbe8b
JaeBotchway <jacqueline.botchway@amalitech.org>
2021-08-29 17:36:09 +0000

    created an array for my database

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+const envelope = require('./src/controllers/routes/envelope')
+
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+  })
+
+  app.use(express.json())
+  app.use(express.urlencoded({ extended: false}))
+
+  app.use('/envelopes', envelope)
+  
+  app.listen(PORT, () => {
+    console.log(`Listening at ${PORT}`)
+  })
